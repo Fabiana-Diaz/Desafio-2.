907533ba5c0c474da6b578af227cb4210e60c827
DANIELA CARVAJALINO <dhayhib.carvajalino@udea.edu.co>
2025-05-26 09:47:02 -0500

    Add files via upload

---
+++ fecha.h
@@ -0,0 +1,13 @@
+#ifndef FECHA_H
+#define FECHA_H
+
+class Fecha {
+private:
+    int dia;
+    int mes;
+    int anio;
+
+    static const char* nombresDias[7];
+    static const char* nombresMeses[12];
+
+#endif // FECHA_H
