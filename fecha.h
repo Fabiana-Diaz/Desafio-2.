d555434379603fc6abc5a26cc9e9a787d5e30ba7
DANIELA CARVAJALINO <dhayhib.carvajalino@udea.edu.co>
2025-05-26 09:47:43 -0500

    Update fecha.h

--- fecha.h
+++ fecha.h
@@ -9,5 +9,17 @@ private:
 
     static const char* nombresDias[7];
     static const char* nombresMeses[12];
+public:
+    // Constructores
+    Fecha(); // Fecha por defecto
+    Fecha(int d, int m, int a);
 
+    // Métodos de acceso
+    int getDia() const;
+    int getMes() const;
+    int getAnio() const;
+
+    void setDia(int d);
+    void setMes(int m);
+    void setAnio(int a);
 #endif // FECHA_H
